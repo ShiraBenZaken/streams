76652c6340529f8b376452dd0d48bb4fb4d1569e
SHIRA BEN ZAKEN <shira441192@gmail.com>
2021-11-21 19:31:48 +0200

    added client folders and start stream

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+export default App;
